65bcfc1436434d2013a29ed2c8440cd0808e8b6c
parksungho <parksungho86@gmail.com>
2014-11-25 19:46:00 +0900

    refactor

---
+++ src/com/youngsterblues/board/BoardServlet.java
@@ -0,0 +1,30 @@
+package com.youngsterblues.board;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/board/*")
+public class BoardServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String[] path = req.getPathInfo().split("/");
+
+		RequestDispatcher dispatcher = req
+				.getRequestDispatcher("/template/board.jsp");
+		if (path.length > 1)
+			req.setAttribute("type", path[1]);
+		if (path.length > 2)
+			req.setAttribute("content", path[2]);
+		dispatcher.forward(req, resp);
+
+	}
+}
